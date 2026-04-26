ae879e26511b5669c86c89a130dd7fe9707c0b86
Jeevant Prakhar Singh <anupamasinghsrinet1976@gmail.com>
2026-04-27 00:53:45 +0530

    added tavily search in frontend

--- src/langgraph_agentic_ai/ui/streamlit/loadUI.py
+++ src/langgraph_agentic_ai/ui/streamlit/loadUI.py
@@ -33,6 +33,10 @@ class LoadStreamlitUI :
                 
             #Usecase Selection
             self.user_controls['selected_usecase'] = st.selectbox("Select Usecase", usecase_options)
-        
+            
+            if self.user_controls["selected_usecase"] == "Research using Web":
+                # Additional controls for Research using Web use case
+                self.user_controls["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"] = st.text_input("TAVILY API KEY" , type="password")
+
         return self.user_controls
     
